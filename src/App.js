9d3f7fe19559fbabfc71e4dbd65321f01812fb6b
facundosegarra <facundosegarra@outlook.com>
2017-03-14 18:32:28 -0300

    configurando y probando el inicio de la app.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import { Button } from 'semantic-ui-react'
+
+function handleSubmit(event) {
+    alert('A name was submitted: ');
+    event.preventDefault();
+}
+
+const ButtonExampleEmphasis = () => (
+    <div>
+        <Button primary onClick={ handleSubmit }>Primary</Button>
+        <Button secondary>Secondary</Button>
+    </div>
+)
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <ButtonExampleEmphasis />
+        );
+    }
 }
 
 export default App;
